67af67ce7bd717df181d62b2eea8b48b16d9bd0f
blengdogamer <40428498+blengdogamer@users.noreply.github.com>
2018-08-27 00:04:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -468,187 +468,86 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
 	const serverQueue = queue.get(msg.guild.id);
 
 	console.log(video);
-
-	
-
-
-
 	const song = {
-
 		id: video.id,
-
 		title: Util.escapeMarkdown(video.title),
-
 		url: `https://www.youtube.com/watch?v=${video.id}`
-
 	};
-
 	if (!serverQueue) {
-
 		const queueConstruct = {
-
 			textChannel: msg.channel,
-
 			voiceChannel: voiceChannel,
-
 			connection: null,
-
 			songs: [],
-
 			volume: 5,
-
 			playing: true
-
 		};
-
 		queue.set(msg.guild.id, queueConstruct);
-
-
-
 		queueConstruct.songs.push(song);
-
-
-
 		try {
-
 			var connection = await voiceChannel.join();
-
 			queueConstruct.connection = connection;
-
 			play(msg.guild, queueConstruct.songs[0]);
-
 		} catch (error) {
-
 			console.error(`I could not join the voice channel: ${error}!`);
-
 			queue.delete(msg.guild.id);
-
 			return msg.channel.send(`Can't join this channel: ${error}!`);
-
 		}
-
 	} else {
-
 		serverQueue.songs.push(song);
-
 		console.log(serverQueue.songs);
-
 		if (playlist) return undefined;
-
 		else return msg.channel.send(`**${song.title}**, just added to the queue! `);
-
 	} 
-
 	return undefined;
-
 }
-
-
-
 function play(guild, song) {
-
 	const serverQueue = queue.get(guild.id);
-
-
-
 	if (!song) {
-
 		serverQueue.voiceChannel.leave();
-
 		queue.delete(guild.id);
-
 		return;
-
 	}
-
 	console.log(serverQueue.songs);
-
-
-
 	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-
 		.on('end', reason => {
-
 			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
-
 			else console.log(reason);
-
 			serverQueue.songs.shift();
-
 			play(guild, serverQueue.songs[0]);
-
 		})
-
 		.on('error', error => console.error(error));
-
 	dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-
-
-
 	serverQueue.textChannel.send(`**${song.title}**, is now playing!`);
 
 }
-
-
-
-
-
 client.on('message', message => {
-
     if (message.content === 'help') {
-
         let helpEmbed = new Discord.RichEmbed()
-
         .setTitle('**أوامر الميوزك...**')
-
         .setDescription('**برفكس البوت (!)**')
-
         .addField('play', 'لتشغيل اغنية')
-
         .addField('join', 'دخول رومك الصوتي')
-
         .addField('disconnect', 'الخروج من رومك الصوتي')
-
         .addField('skip', 'تخطي الأغنية')
-
         .addField('pause', 'ايقاف الاغنية مؤقتا')
-
         .addField('resume', 'تكملة الاغنية')
-
         .addField('queue', 'اظهار قائمة التشغيل')
-
         .addField('np', 'اظهار الاغنية اللي انت مشغلها حاليا')
-
         .setFooter('(general_commands) لاظهار الاوامر العامة')
-
       message.channel.send(helpEmbed);
-
     }
-
 });
-
-
-
 client.on('message', message => {
-
     if (message.content === 'general_commands') {
-
         let helpEmbed = new Discord.RichEmbed()
-
         .setTitle('**أوامر عامة...**')
-
         .addField('avatar', "افاتار الشخص المطلوب")
-
         .addField('gif', 'البحث عن جيف انت تطلبه')
-
         .addField('ping', 'معرفة ping البوت')
-
         .setFooter('المزيد قريبا ان شاء الله!')
-
       message.channel.send(helpEmbed);
-
     }
-
 });
 
 
